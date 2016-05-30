fb0c8da8bee65657bdf19ae94d7f9fb494c983de
Вадим <guts.vadim@gmail.com>
2016-05-30 13:48:31 +0300

    down culumns type fix

--- migrations/m160529_200721_remove_seo_columns.php
+++ migrations/m160529_200721_remove_seo_columns.php
@@ -21,16 +21,16 @@ class m160529_200721_remove_seo_columns extends Migration
 
     public function safeDown()
     {
-        $this->addColumn('shop_product_translation', 'seoUrl', 'string');
-        $this->addColumn('shop_product_translation', 'seoTitle', 'string');
-        $this->addColumn('shop_product_translation', 'seoDescription', 'string');
-        $this->addColumn('shop_product_translation', 'seoKeywords', 'string');
-        $this->addColumn('shop_product_translation', 'full_text', 'string');
+        $this->addColumn('shop_product_translation', 'seoUrl', $this->string());
+        $this->addColumn('shop_product_translation', 'seoTitle', $this->string());
+        $this->addColumn('shop_product_translation', 'seoDescription', $this->string());
+        $this->addColumn('shop_product_translation', 'seoKeywords', $this->string());
+        $this->addColumn('shop_product_translation', 'full_text', $this->string());
 
-        $this->addColumn('shop_category_translation', 'seoUrl', 'string');
-        $this->addColumn('shop_category_translation', 'seoTitle', 'string');
-        $this->addColumn('shop_category_translation', 'seoDescription', 'string');
-        $this->addColumn('shop_category_translation', 'seoKeywords', 'string');
-        $this->addColumn('shop_category_translation', 'full_text', 'string');
+        $this->addColumn('shop_category_translation', 'seoUrl', $this->string());
+        $this->addColumn('shop_category_translation', 'seoTitle', $this->string());
+        $this->addColumn('shop_category_translation', 'seoDescription', $this->string());
+        $this->addColumn('shop_category_translation', 'seoKeywords', $this->string());
+        $this->addColumn('shop_category_translation', 'full_text', $this->string());
     }
 }
