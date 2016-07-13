5f2e284803b67f95f2f161bcc3ae0798f82132c8
Vyacheslav Nozhenko <vv.nojenko@gmail.com>
2016-07-13 18:11:59 +0300

    backend: Refactor + modify views of Vendor.

--- backend/views/vendor/list.php
+++ backend/views/vendor/list.php
@@ -1,5 +1,5 @@
 <?php
-use bl\cms\shop\backend\components\form\VendorImageForm;
+use bl\cms\shop\backend\components\form\VendorImage;
 use bl\cms\shop\common\entities\Vendor;
 use yii\bootstrap\Html;
 use yii\helpers\Url;
@@ -8,9 +8,10 @@ use yii\helpers\Url;
  * @author Gutsulyak Vadim <guts.vadim@gmail.com>
  *
  * @var Vendor[] $vendors
- * @var VendorImageForm $image_form
+ * @var VendorImage $vendor_images
  */
 
+$this->title = Yii::t('shop', 'Vendor List');
 ?>
 
 <div class="row">
@@ -18,64 +19,75 @@ use yii\helpers\Url;
         <div class="panel panel-default">
             <div class="panel-heading">
                 <i class="glyphicon glyphicon-list"></i>
-                <?= 'Vendor list' ?>
+                <?= Html::encode($this->title); ?>
             </div>
-            <div class="panel-body">
-                <table class="table table-hover">
-                    <? if (!empty($vendors)): ?>
-                        <thead>
-                        <tr>
-                            <th><?= 'Id' ?></th>
-                            <th><?= 'Image' ?></th>
-                            <th><?= 'Title' ?></th>
-                            <th>Edit</th>
-                            <th>Delete</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        <? foreach ($vendors as $vendor): ?>
+            <div>
+                <div class="panel-body">
+                    <table class="table-bordered table-condensed table-hover">
+                        <?php if (!empty($vendors)): ?>
+                            <thead>
                             <tr>
-                                <td>
-                                    <?= $vendor->id ?>
-                                </td>
-
-                                <td>
-                                    <?php if(!empty($vendor->image_name)): ?>
-                                        <?= Html::img($image_form->getSmall($vendor->image_name)) ?>
-                                    <?php endif; ?>
-                                </td>
-
-                                <td>
-                                    <?= $vendor->title ?>
-                                </td>
-
-                                <td>
-                                    <?= Html::a('', [
-                                        'save',
-                                        'id' => $vendor->id
-                                    ], [
-                                        'class' => 'glyphicon glyphicon-edit text-warning btn btn-default btn-sm'
-                                    ]); ?>
-                                </td>
-
-                                <td>
-                                    <?= Html::a('', [
-                                        'remove',
-                                        'id' => $vendor->id
-                                    ], [
-                                        'class' => 'glyphicon glyphicon-remove text-danger btn btn-default btn-sm'
-                                    ]); ?>
-                                </td>
+                                <th><?= 'Id' ?></th>
+                                <th class="col-sm-1 col-md-2"><?= 'Image' ?></th>
+                                <th class="col-md-12 col-sm-12 col-xs-12"><?= 'Title' ?></th>
+                                <th>Edit</th>
+                                <th>Delete</th>
                             </tr>
-                        <? endforeach; ?>
-                        </tbody>
-                    <? endif; ?>
-                </table>
+                            </thead>
+                            <tbody>
+                            <?php foreach ($vendors as $vendor): ?>
+                                <tr>
+                                    <td>
+                                        <?= $vendor->id ?>
+                                    </td>
 
-                <a href="<?= Url::to(['save']); ?>"
-                   class="btn btn-primary pull-right">
-                    <i class="fa fa-user-plus"></i> <?= 'Add' ?>
-                </a>
+                                    <td>
+                                        <?php if (!empty($vendor->image_name)): ?>
+                                            <a href="<?= $vendor_images->getBig($vendor->image_name) ?>" target="blank">
+                                                <?= Html::img(
+                                                    $vendor_images->getBig($vendor->image_name),
+                                                    ['class' => 'img-responsive']
+                                                )?>
+                                            </a>
+                                        <?php endif; ?>
+                                    </td>
+
+                                    <td>
+                                        <?= $vendor->title ?>
+                                    </td>
+
+                                    <td>
+                                        <?= Html::a('', [
+                                            'save',
+                                            'id' => $vendor->id
+                                        ], [
+                                            'class' => 'glyphicon glyphicon-edit text-warning btn btn-warning btn-sm'
+                                        ]); ?>
+                                    </td>
+
+                                    <td>
+                                        <div class="text-center">
+                                        <?= Html::a('', [
+                                            'remove',
+                                            'id' => $vendor->id
+                                        ], [
+                                            'class' => 'glyphicon glyphicon-remove text-danger btn btn-default btn-sm'
+                                        ]); ?>
+                                        </div>
+                                    </td>
+                                </tr>
+                            <?php endforeach; ?>
+                            </tbody>
+                        <? endif; ?>
+                    </table>
+                    <div class="row-fluid" style="margin-top: 15px;">
+                        <a href="<?= Url::to(['save']); ?>"
+                           class="btn btn-primary pull-right">
+                            <i class="fa fa-user-plus"></i> <?= Yii::t('yii', 'Add'); ?>
+                        </a>
+                    </div>
+                </div>
+
             </div>
         </div>
     </div>
