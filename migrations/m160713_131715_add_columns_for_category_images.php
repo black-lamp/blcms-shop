9860faa7b45ff3a6185f05a12b02fe85a95cb51d
xalberteinsteinx <xalbert.einsteinx@gmail.com>
2016-07-13 17:48:38 +0300

    Migration fixed.

--- migrations/m160713_131715_add_columns_for_category_images.php
+++ migrations/m160713_131715_add_columns_for_category_images.php
@@ -6,7 +6,6 @@ class m160713_131715_add_columns_for_category_images extends Migration
 {
     public function up()
     {
-        $this->dropColumn('shop_category', 'image_name');
         $this->addColumn('shop_category', 'cover', 'string');
         $this->addColumn('shop_category', 'thumbnail', 'string');
         $this->addColumn('shop_category', 'menu_item', 'string');
@@ -14,7 +13,6 @@ class m160713_131715_add_columns_for_category_images extends Migration
 
     public function down()
     {
-        $this->addColumn('shop_category', 'image_name', 'string');
         $this->dropColumn('shop_category', 'cover');
         $this->dropColumn('shop_category', 'thumbnail');
         $this->dropColumn('shop_category', 'menu_item');
