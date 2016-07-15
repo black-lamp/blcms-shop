344ec85cbd82499a5e0d8e5d8960c869dd23a6c8
xalberteinsteinx <xalbert.einsteinx@gmail.com>
2016-07-15 14:21:43 +0300

    Translations added to categories.

--- backend/views/category/index.php
+++ backend/views/category/index.php
@@ -8,41 +8,41 @@ use yii\widgets\Pjax;
 /* @var $categories CategoryTranslation */
 /* @var $languages Language[] */
 
-$this->title = 'Product category list';
+$this->title = \Yii::t('shop', 'Product categories');
 ?>
 
 <? Pjax::begin([
     'linkSelector' => '.category-nav',
     'enablePushState' => false
 ]) ?>
-<h1>Categories</h1>
+<h1><?=\Yii::t('shop', 'Product categories'); ?></h1>
 <div class="row">
     <div class="col-md-12">
         <div class="panel panel-default">
             <div class="panel-heading">
                 <i class="glyphicon glyphicon-list"></i>
-                <?= 'Categories list'?>
+                <?= \Yii::t('shop', 'Categories list'); ?>
             </div>
             <div class="panel-body">
                 <table class="table table-hover">
                     <? if(!empty($categories)): ?>
                         <thead>
                         <tr>
-                            <th><?= 'Position'; ?></th>
-                            <th class="col-md-3"><?= 'Name'?></th>
-                            <th class="col-md-2"><?= 'Parent name'?></th>
+                            <th class="col-md-1 text-center"><?= \Yii::t('shop', 'Position'); ?></th>
+                            <th class="col-md-3 text-center"><?= \Yii::t('shop', 'Name'); ?></th>
+                            <th class="col-md-3 text-center"><?= \Yii::t('shop', 'Parent'); ?></th>
                             <? if(count($languages) > 1): ?>
-                                <th class="col-lg-3"><?= 'Language' ?></th>
+                                <th class="col-md-2 text-center"><?= \Yii::t('shop', 'Language'); ?></th>
                             <? endif; ?>
-                            <th class="text-center">Show</th>
-                            <th>Edit</th>
-                            <th>Delete</th>
+                            <th class="col-md-1 text-center"><?= \Yii::t('shop', 'Show'); ?></th>
+                            <th class="col-md-1 text-center"><?= \Yii::t('shop', 'Edit'); ?></th>
+                            <th class="col-md-1 text-center"><?= \Yii::t('shop', 'Delete'); ?></th>
                         </tr>
                         </thead>
                         <tbody>
                         <? foreach($categories as $category): ?>
                             <tr>
-                                <td>
+                                <td class="text-center">
                                     <?= $category->position ?>
                                     <a href="<?= Url::to([
                                         'up',
@@ -92,14 +92,14 @@ $this->title = 'Product category list';
                                     </a>
                                 </td>
 
-                                <td>
+                                <td class="text-center">
                                     <a href="<?= Url::to(['save', 'categoryId' => $category->id, 'languageId' => Language::getCurrent()->id])?>"
                                        class="glyphicon glyphicon-edit text-warning btn btn-default btn-sm">
                                     </a>
                                     <br>
                                 </td>
 
-                                <td>
+                                <td class="text-center">
                                     <a href="<?= Url::to(['delete', 'id' => $category->id])?>"
                                        class="category-nav glyphicon glyphicon-remove text-danger btn btn-default btn-sm">
                                     </a>
