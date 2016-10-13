418be15043bf7912e07f8e03e8c3a0272ad94d16
xalberteinsteinx <xalbert.einsteinx@gmail.com>
2016-10-13 17:05:47 +0300

    Manage buttons changed to widget.

--- backend/views/category/index.php
+++ backend/views/category/index.php
@@ -1,6 +1,7 @@
 <?php
 
 use bl\cms\shop\common\entities\Category;
+use bl\cms\shop\widgets\ManageButtons;
 use bl\multilang\entities\Language;
 use yii\helpers\ArrayHelper;
 use yii\helpers\Html;
@@ -156,45 +157,7 @@ $this->params['breadcrumbs'][] = $this->title;
                     'attribute' => \Yii::t('shop', 'Manage'),
 
                     'value' => function ($model) {
-                        global $category;
-                        $category = $model;
-
-                        $languages = Language::findAll(['active' => true]);
-                        $list =
-                            Html::a('<span class="glyphicon glyphicon-remove"></span>', Url::toRoute(['delete', 'id' => $GLOBALS['category']->id]),
-                                ['title' => Yii::t('yii', 'Delete'), 'class' => 'btn btn-danger pull-right pjax']) .
-
-                            Html::tag('div',
-                                Html::a(
-                                    'Edit',
-                                    Url::toRoute(['save', 'categoryId' => $GLOBALS['category']->id, "languageId" => Language::getCurrent()->id]),
-                                    [
-                                        'class' => 'col-md-8 btn btn-default ',
-                                    ]) .
-                                Html::a(
-                                    '<span class="caret"></span>',
-                                    Url::toRoute(['save', 'categoryId' => $GLOBALS['category']->id, "languageId" => Language::getCurrent()->id]),
-                                    [
-                                        'class' => 'block col-md-4 btn btn-default dropdown-toggle',
-                                        'type' => 'button', 'id' => 'dropdownMenu1',
-                                        'data-toggle' => 'dropdown', 'aria-haspopup' => 'true',
-                                        'aria-expanded' => 'true'
-                                    ]) .
-                                Html::ul(
-                                    ArrayHelper::map($languages, 'id', 'name'),
-                                    [
-                                        'item' => function ($item, $index) {
-                                            return Html::tag('li',
-                                                Html::a($item, Url::toRoute(['save', 'categoryId' => $GLOBALS['category']->id, "languageId" => $index]), []),
-                                                []
-                                            );
-                                        },
-                                        'class' => 'dropdown-menu', 'aria-labelledby' => 'dropdownMenu1']),
-
-                                ['class' => 'btn-group pull-left']
-                            );
-
-                        return $list;
+                        return ManageButtons::widget(['model' => $model]);
                     },
                     'format' => 'raw',
                     'contentOptions' => ['class' => 'col-md-2 text-center'],
