06593b208f67187f800a7afd9402f72cf6c352e1
xalberteinsteinx <xalbert.einsteinx@gmail.com>
2016-10-20 16:57:31 +0300

    Save and delete methods added to ProductAvailabilityController.

--- backend/controllers/ProductAvailabilityController.php
+++ backend/controllers/ProductAvailabilityController.php
@@ -2,8 +2,12 @@
 namespace bl\cms\shop\backend\controllers;
 
 use bl\cms\shop\common\entities\ProductAvailability;
+use bl\cms\shop\common\entities\ProductAvailabilityTranslation;
 use bl\multilang\entities\Language;
+use Yii;
+use yii\web\BadRequestHttpException;
 use yii\web\Controller;
+use yii\web\NotFoundHttpException;
 
 /**
  * @author Albert Gainutdinov <xalbert.einsteinx@gmail.com>
@@ -19,4 +23,108 @@ class ProductAvailabilityController extends Controller
         ]);
     }
 
+    /**
+     * Creates a new DeliveryMethod model.
+     * If creation is successful, the browser will be redirected to the 'view' page.
+     * @param integer $id
+     * @param integer $languageId
+     * @return mixed
+     * @throws NotFoundHttpException
+     * @throws BadRequestHttpException
+     */
+    public function actionSave($id = null, $languageId)
+    {
+        if (!empty($languageId)) {
+            if (!empty($id)) {
+
+                $model = $this->findModel($id);
+                if (empty($model)) {
+                    throw new NotFoundHttpException;
+                }
+                $modelTranslation = $this->findModelTranslation($id, $languageId);
+                if (empty($modelTranslation)) {
+                    $modelTranslation = new ProductAvailabilityTranslation();
+                }
+            }
+            else {
+                $model = new ProductAvailability();
+                $modelTranslation = new ProductAvailabilityTranslation();
+            }
+        }
+        else throw new BadRequestHttpException();
+
+
+        if ($modelTranslation->load(Yii::$app->request->post())) {
+
+            $model->save(false);
+
+            $modelTranslation->availability_id = $model->id;
+            $modelTranslation->language_id = $languageId;
+            if ($modelTranslation->validate()) {
+
+                $modelTranslation->save();
+                return $this->redirect(['save', 'id' => $model->id, 'languageId' => $languageId]);
+            }
+        }
+        return $this->render('save', [
+            'model' => $model,
+            'modelTranslation' => $modelTranslation,
+            'languages' => Language::find()->all(),
+            'selectedLanguage' => Language::findOne($languageId)
+        ]);
+
+    }
+
+
+    /**
+     * Deletes an existing DeliveryMethod model.
+     * If deletion is successful, the browser will be redirected to the 'index' page.
+     * @param integer $id
+     * @return mixed
+     */
+    public function actionDelete($id)
+    {
+        $this->findModel($id)->delete();
+
+        return $this->redirect(['index']);
+    }
+
+    /**
+     * Finds the DeliveryMethod model based on its primary key value.
+     * If the model is not found, a 404 HTTP exception will be thrown.
+     * @param integer $id
+     * @return ProductAvailability the loaded model
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    protected function findModel($id)
+    {
+        if (($model = ProductAvailability::findOne($id)) !== null) {
+            return $model;
+        } else {
+            throw new NotFoundHttpException('The requested page does not exist.');
+        }
+    }
+
+
+    /**
+     * Finds the DeliveryMethodTranslation model based on delivery method id and language id.
+     * If the model is not found, a 404 HTTP exception will be thrown.
+     * @param integer $id
+     * @param integer $languageId
+     * @return mixed
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    protected function findModelTranslation($id, $languageId)
+    {
+        $model = ProductAvailabilityTranslation::find()->where([
+            'availability_id' => $id,
+            'language_id' => $languageId
+        ])->one();
+
+        if ($model !== null) {
+            return $model;
+        } else {
+            return false;
+        }
+    }
 }
--- backend/views/product-availability/save.php
+++ backend/views/product-availability/save.php
@@ -9,13 +9,13 @@ use yii\helpers\Url;
  * @author Albert Gainutdinov <xalbert.einsteinx@gmail.com>
  *
  * @var $this yii\web\View
- * @var $model bl\cms\cart\models\DeliveryMethod
- * @var $modelTranslation bl\cms\cart\models\DeliveryMethodTranslation
+ * @var $model bl\cms\shop\common\entities\ProductAvailability
+ * @var $modelTranslation bl\cms\shop\common\entities\ProductAvailabilityTranslation
  * @var $languages \bl\multilang\entities\Language[]
  * @var $selectedLanguage \bl\multilang\entities\Language
  */
 
-$this->title = Yii::t('shop', 'Delivery method');
+$this->title = Yii::t('shop', 'Product availabilities');
 ?>
 
 
@@ -62,25 +62,9 @@ $this->title = Yii::t('shop', 'Delivery method');
         <?php $form = ActiveForm::begin(); ?>
         <?= $form->field($modelTranslation, 'title')->textInput(['maxlength' => true]); ?>
         <?= $form->field($modelTranslation, 'description')->widget(Summernote::className()); ?>
-        <?= $form->field($model, 'show_address_or_post_office')->dropDownList([
-            $model::DO_NOT_SHOW_ADDRESS_OR_POST_OFFICE_FIELDS => \Yii::t('shop', 'Show nothing'),
-            $model::SHOW_ADDRESS_FIELDS => \Yii::t('shop', 'Show address fields'),
-            $model::SHOW_POST_OFFICE_FIELD => \Yii::t('shop', 'Show post office field')
-        ]); ?>
-
-        <div class="row">
-            <div class="col-md-9">
-                <?= $form->field($model, 'logo')->fileInput(); ?>
-            </div>
-            <div class="col-md-3">
-                <?php if (!empty($model->smallLogo)) : ?>
-                    <?= Html::img($model->smallLogo); ?>
-                <?php endif; ?>
-            </div>
-        </div>
 
         <div class="form-group">
-            <?= Html::a(Yii::t('shop', 'Close'), Url::toRoute('delivery-method/index'), ['class' => 'btn btn-xs btn-danger pull-right']); ?>
+            <?= Html::a(Yii::t('shop', 'Close'), Url::toRoute('product-availability/index'), ['class' => 'btn btn-xs btn-danger pull-right']); ?>
             <?= Html::submitButton(Yii::t('shop', 'Save'), ['class' => 'btn btn-xs btn-primary pull-right m-r-xs']); ?>
         </div>
         <?php ActiveForm::end(); ?>
