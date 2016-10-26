0b565feeececa78bc71911ea9c19da8930974d65
xalberteinsteinx <xalbert.einsteinx@gmail.com>
2016-10-26 11:39:45 +0300

    CartController fix.

--- frontend/controllers/CartController.php
+++ frontend/controllers/CartController.php
@@ -139,7 +139,7 @@ class CartController extends Controller
             $profile = Profile::find()->where(['user_id' => \Yii::$app->user->id])->one();
             $user = $profile->user;
             $order = Order::find()->where(['user_id' => \Yii::$app->user->id, 'status' => CartComponent::STATUS_INCOMPLETE])->one();
-            $address = $order->address;
+            $address = (!empty($order->address)) ? $order->address : '';
 
             $post = Yii::$app->request->post();
             if(\Yii::$app->cart->makeOrder($post)) {
@@ -198,5 +198,57 @@ class CartController extends Controller
                 ]
             ]);
         }
+    }
+
+    public function actionGetAreasFromNp() {
+        return $this->getResponse('Address', 'getAreas');
+    }
+
+    public function actionGetCitiesFromNp() {
+        return $this->getResponse('AddressGeneral', 'getSettlements');
+    }
+
+    public function actionGetWarehousesFromNp($cityName) {
+
+//        $cityName = (!empty($cityName)) ? $cityName : $this->defaultCityName;
+
+        $methodProperties = [
+            'CityName' => $cityName
+        ];
+
+        return $this->getResponse('AddressGeneral', 'getWarehouses', $methodProperties);
+    }
+
+
+
+    /**
+     * This method is used for Nova Poshta widget.
+     *
+     * @param $modelName
+     * @param $calledMethod
+     * @param null $methodProperties
+     * @return string
+     */
+    private function getResponse($modelName, $calledMethod, $methodProperties = null) {
+
+        $data = [
+            'apiKey' => $this->token,
+            'modelName' => $modelName,
+            'calledMethod' => $calledMethod,
+            'language' => $this->language,
+            'methodProperties' => $methodProperties
+        ];
+
+        $post = json_encode($data);
+
+        $result = file_get_contents($this->url, null, stream_context_create([
+            'http' => [
+                'method' => 'POST',
+                'header' => "Content-type: application/x-www-form-urlencoded;\r\n",
+                'content' => $post,
+            ]
+        ]));
+
+        return $result;
     }
 }
