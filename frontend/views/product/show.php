dc291f89bfa0b84a73b3c982d4ed10ce96472f6a
xalberteinsteinx <xalbert.einsteinx@gmail.com>
2016-10-26 15:35:57 +0300

    View fix.

--- frontend/views/product/show.php
+++ frontend/views/product/show.php
@@ -24,153 +24,163 @@ use yii\widgets\Breadcrumbs;
 ProductAsset::register($this);
 ?>
 
-<!--BREADCRUMBS-->
-<div>
-    <?php echo Breadcrumbs::widget([
-        'itemTemplate' => "<li><b><span>{link}</span></b></li>\n",
-        'homeLink' => [
-            'label' => Yii::t('frontend/navigation', 'Главная'),
-            'url' => Url::toRoute(['/']),
+    <!--BREADCRUMBS-->
+    <div>
+        <?php
+        $shop = [
+            'label' => Yii::t('frontend/navigation', 'Магазин'),
+            'url' => Url::toRoute(['/shop']),
             'itemprop' => 'url',
-        ],
-        'links' => [
+        ];
+        $category = (!empty($category->translation->title)) ?
             [
-                'label' => Yii::t('frontend/navigation', 'Магазин'),
-                'url' => Url::toRoute(['/shop']),
-                'itemprop' => 'url',
-            ],
-            [
-                'label' => $category->translation->title,
+                'label' => (!empty($category->translation->title)),
                 'url' => Url::toRoute(['category/show', 'id' => $category->id]),
                 'itemprop' => 'url',
+            ] : [];
+        $item = (!empty($product->translation->title)) ? $product->translation->title : '';
+
+        $links = (!empty($category)) ? [
+            $shop, $category, $product
+        ] : [$shop, $item];
+
+        echo Breadcrumbs::widget([
+            'itemTemplate' => "<li><b><span>{link}</span></b></li>\n",
+            'homeLink' => [
+                'label' => Yii::t('frontend/navigation', 'Главная'),
+                'url' => Url::toRoute(['/']),
+                'itemprop' => 'url',
             ],
-            $product->translation->title,
-        ],
-    ]);
-    ?>
-</div>
+            'links' => $links
+        ]);
+        ?>
+    </div>
 
-<!--ALERT-->
-<div class="col-lg-12">
-    <?php if (Yii::$app->session->hasFlash('alert')): ?>
-        <div class="alert alert-success" role="alert">
-            <?= Yii::$app->session->getFlash('alert') ?>
-        </div>
-    <?php endif; ?>
-</div>
+    <!--ALERT-->
+    <div class="col-lg-12">
+        <?php if (Yii::$app->session->hasFlash('alert')): ?>
+            <div class="alert alert-success" role="alert">
+                <?= Yii::$app->session->getFlash('alert') ?>
+            </div>
+        <?php endif; ?>
+    </div>
 
-<!--PRODUCT CARD-->
-<div class="row product-page">
+    <!--PRODUCT CARD-->
+    <div class="row product-page">
 
-    <h1 class="col-md-12 text-center"><?= $product->translation->title ?></h1>
+        <h1 class="col-md-12 text-center"><?= (!empty($product->translation->title))
+                ? $product->translation->title : '' ?>
+        </h1>
 
-    <div class="col-md-4 image">
-        <!--IMAGE-->
+        <div class="col-md-4 image">
+            <!--IMAGE-->
             <?= Html::img(
                 (!empty($product->images)) ? $product->image->thumb : Url::toRoute('/images/default.jpg'),
                 [
-                'class' => 'media-object img-responsive',
-                'alt' => (!empty($product->images)) ? Html::encode($product->image->alt) : $product->translation->title
-            ]); ?>
+                    'class' => 'media-object img-responsive',
+                    'alt' => (!empty($product->images)) ?
+                        Html::encode($product->image->alt) :
+                        ''
+                ]); ?>
 
-    </div>
-
-
-    <div class="col-md-8">
-        <!--ARTICULUS-->
-        <?php if (!empty($product->articulus)) : ?>
-            <div class="intro-text">
-                <p>
-                    <strong><?=\Yii::t('shop', 'SKU'); ?></strong>: <?= $product->articulus; ?>
-                </p>
-            </div>
-        <?php endif ?>
-
-        <!--VENDOR-->
-        <?php if (!empty($product->vendor)) : ?>
-            <div class="intro-text">
-                <p>
-                    <strong><?=\Yii::t('shop', 'Vendor'); ?></strong>: <?= $product->vendor->title; ?>
-                </p>
-            </div>
-        <?php endif ?>
-
-        <!--COUNTRY-->
-        <?php if (!empty($product->productCountry)) : ?>
-            <p>
-                <strong><?=\Yii::t('shop', 'Country'); ?></strong>: <?= $product->productCountry->translation->title; ?>
-            </p>
-        <?php endif; ?>
-
-        <!--AVAILABILITY-->
-        <?php if (!empty($product->availability)) : ?>
-            <div class="availability">
-                <p class="">
-                    <strong><?= $product->productAvailability->translation->title; ?></strong>
-                </p>
-                <p>
-                    <?= $product->productAvailability->translation->description; ?>
-                </p>
-            </div>
-        <?php endif; ?>
-
-        <!-- DESCRIPTION -->
-        <p class="article-label"><?= Yii::t('shop', 'Description'); ?></p>
-        <?php if (!empty($product->translation->description)) : ?>
-            <div class="description">
-                <?= $product->translation->description ?>
-            </div>
-        <?php endif ?>
-
-        <?php $form = ActiveForm::begin([
-            'method' => 'post',
-            'action' => ['/shop/cart/add']
-        ]); ?>
-
-        <!--PRICES-->
-        <?php if (!empty($product->prices)) : ?>
-            <?= $form->field($cart, 'priceId', ['options' => ['class' => 'col-md-4']])
-                ->dropDownList(ArrayHelper::map($product->prices, 'id',
-                    function ($model) {
-                        $priceItem = $model->translation->title . ' - ' . \Yii::$app->formatter->asCurrency($model->salePrice);
-                        return $priceItem;
-                }))
-                ->label(\Yii::t('shop', 'Price'));
-            ?>
-        <?php elseif (!empty($product->price)) : ?>
-            <?= \Yii::$app->formatter->asCurrency($product->price); ?>
-        <?php endif; ?>
-
-        <!--QUANTITY-->
-        <?= $form->field($cart, 'count', ['options' => ['class' => 'col-md-4']])->
-        textInput([
-            'type' => 'number',
-            'min' => '1',
-            'value' => '1',
-            'data-action' => 'text',
-            'class' => 'form-control',
-            'id' => 'count'
-        ])->label(\Yii::t('shop', 'Count'));
-        ?>
-        <!--PRODUCT ID HIDDEN INPUT-->
-        <?= $form->field($cart, 'productId')->hiddenInput(['value' => $product->id])->label(false); ?>
-
-        <?= Html::submitButton(Yii::t('shop', 'Add to cart'),
-            [
-                'class' => 'btn btn-primary'
-            ]); ?>
-
-        <?php $form::end(); ?>
-
-    </div>
-
-    <!--FULL TEXT -->
-    <?php if (!empty($product->translation->full_text)) : ?>
-        <div class="full-text">
-            <?= $product->translation->full_text ?>
         </div>
-    <?php endif ?>
-</div>
+
+
+        <div class="col-md-8">
+            <!--ARTICULUS-->
+            <?php if (!empty($product->articulus)) : ?>
+                <div class="intro-text">
+                    <p>
+                        <strong><?= \Yii::t('shop', 'SKU'); ?></strong>: <?= $product->articulus; ?>
+                    </p>
+                </div>
+            <?php endif ?>
+
+            <!--VENDOR-->
+            <?php if (!empty($product->vendor)) : ?>
+                <div class="intro-text">
+                    <p>
+                        <strong><?= \Yii::t('shop', 'Vendor'); ?></strong>: <?= $product->vendor->title; ?>
+                    </p>
+                </div>
+            <?php endif ?>
+
+            <!--COUNTRY-->
+            <?php if (!empty($product->productCountry)) : ?>
+                <p>
+                    <strong><?= \Yii::t('shop', 'Country'); ?></strong>: <?= $product->productCountry->translation->title; ?>
+                </p>
+            <?php endif; ?>
+
+            <!--AVAILABILITY-->
+            <?php if (!empty($product->availability)) : ?>
+                <div class="availability">
+                    <p class="">
+                        <strong><?= $product->productAvailability->translation->title; ?></strong>
+                    </p>
+                    <p>
+                        <?= $product->productAvailability->translation->description; ?>
+                    </p>
+                </div>
+            <?php endif; ?>
+
+            <!-- DESCRIPTION -->
+            <p class="article-label"><?= Yii::t('shop', 'Description'); ?></p>
+            <?php if (!empty($product->translation->description)) : ?>
+                <div class="description">
+                    <?= $product->translation->description ?>
+                </div>
+            <?php endif ?>
+
+            <?php $form = ActiveForm::begin([
+                'method' => 'post',
+                'action' => ['/shop/cart/add']
+            ]); ?>
+
+            <!--PRICES-->
+            <?php if (!empty($product->prices)) : ?>
+                <?= $form->field($cart, 'priceId', ['options' => ['class' => 'col-md-4']])
+                    ->dropDownList(ArrayHelper::map($product->prices, 'id',
+                        function ($model) {
+                            $priceItem = $model->translation->title . ' - ' . \Yii::$app->formatter->asCurrency($model->salePrice);
+                            return $priceItem;
+                        }))
+                    ->label(\Yii::t('shop', 'Price'));
+                ?>
+            <?php elseif (!empty($product->price)) : ?>
+                <?= \Yii::$app->formatter->asCurrency($product->price); ?>
+            <?php endif; ?>
+
+            <!--QUANTITY-->
+            <?= $form->field($cart, 'count', ['options' => ['class' => 'col-md-4']])->
+            textInput([
+                'type' => 'number',
+                'min' => '1',
+                'value' => '1',
+                'data-action' => 'text',
+                'class' => 'form-control',
+                'id' => 'count'
+            ])->label(\Yii::t('shop', 'Count'));
+            ?>
+            <!--PRODUCT ID HIDDEN INPUT-->
+            <?= $form->field($cart, 'productId')->hiddenInput(['value' => $product->id])->label(false); ?>
+
+            <?= Html::submitButton(Yii::t('shop', 'Add to cart'),
+                [
+                    'class' => 'btn btn-primary'
+                ]); ?>
+
+            <?php $form::end(); ?>
+
+        </div>
+
+        <!--FULL TEXT -->
+        <?php if (!empty($product->translation->full_text)) : ?>
+            <div class="full-text">
+                <?= $product->translation->full_text ?>
+            </div>
+        <?php endif ?>
+    </div>
 
 <?= \bl\cms\shop\widgets\RecommendedProducts::widget([
     'id' => $product->id
