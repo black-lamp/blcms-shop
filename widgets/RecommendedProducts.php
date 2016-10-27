8e7c8a0f6b280f697a170f9d573a245ae73c9484
xalberteinsteinx <xalbert.einsteinx@gmail.com>
2016-10-27 17:38:13 +0300

    Widget fix.

--- widgets/RecommendedProducts.php
+++ widgets/RecommendedProducts.php
@@ -42,31 +42,34 @@ class RecommendedProducts extends Widget
             $product = Product::findOne($id);
             $categoryId = $product->category_id;
 
-            $previous = Product::find()->where(['<', 'id', $id])->andWhere(['category_id' => $categoryId])->orderBy(['id' => SORT_DESC])->limit('2')->all();
-            $next = Product::find()->where(['>', 'id', $id])->andWhere(['category_id' => $categoryId])->orderBy(['id' => SORT_ASC])->limit('2')->all();
-            if (empty($next[1]) && !empty($next[0])) {
+            $previous = Product::find()->where(['<', 'id', $id])
+                ->andWhere(['category_id' => $categoryId])->orderBy(['id' => SORT_DESC])->limit('2')->all();
+            $next = Product::find()->where(['>', 'id', $id])
+                ->andWhere(['category_id' => $categoryId])->orderBy(['id' => SORT_ASC])->limit('2')->all();
+
+            if (!isset($next[1]) && !empty($next[0])) {
                 $next[1] = Product::find()->where(['<', 'id', $id])->andwhere(['category_id' => $categoryId])->orderBy(['id' => SORT_ASC])->one();
             }
-            if (empty($next[0])) {
+            if (isset($next[0])) {
                 $next = Product::find()->where(['<', 'id', $id])->andwhere(['category_id' => $categoryId])->orderBy(['id' => SORT_ASC])->limit('2')->all();
             }
-            if (empty($previous[1]) && !empty($previous[0])) {
+            if (isset($previous[1]) && !empty($previous[0])) {
                 $previous[1] = Product::find()->where(['>', 'id', $id])->andwhere(['category_id' => $categoryId])->orderBy(['id' => SORT_DESC])->one();
             }
-            if (empty($previous[0])) {
+            if (isset($previous[0])) {
                 $previous = Product::find()->where(['>', 'id', $id])->andwhere(['category_id' => $categoryId])->orderBy(['id' => SORT_DESC])->limit('2')->all();
             }
             $recommendedProducts = [];
-            if (!empty($previous[1])) {
+            if (isset($previous[1])) {
                 $recommendedProducts[] = $previous[1];
             }
-            if (!empty($previous[0])) {
+            if (isset($previous[0])) {
                 $recommendedProducts[] = $previous[0];
             }
-            if (!empty($next[0])) {
+            if (isset($next[0])) {
                 $recommendedProducts[] = $next[0];
             }
-            if (!empty($next[1])) {
+            if (isset($next[1])) {
                 $recommendedProducts[] = $next[1];
             }
             return $recommendedProducts;
