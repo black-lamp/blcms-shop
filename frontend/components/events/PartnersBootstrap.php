a8cb0515b3c4213180c6572e8751a9d81939b753
xalberteinsteinx <xalbert.einsteinx@gmail.com>
2016-11-10 12:28:38 +0200

    Adds e-mail notifications functionality to PartnerBootstrap class.

--- frontend/components/events/PartnersBootstrap.php
+++ frontend/components/events/PartnersBootstrap.php
@@ -1,18 +1,14 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: xeinsteinx
- * Date: 02.09.16
- * Time: 16:49
- */
-
 namespace bl\cms\shop\frontend\components\events;
-
-
 use bl\cms\shop\frontend\controllers\PartnerRequestController;
+use Exception;
+use Yii;
 use yii\base\BootstrapInterface;
 use yii\base\Event;
 
+/**
+ * @author Albert Gainutdinov <xalbert.einsteinx@gmail.com>
+ */
 class PartnersBootstrap implements BootstrapInterface
 {
     public function bootstrap($app)
@@ -21,7 +17,30 @@ class PartnersBootstrap implements BootstrapInterface
     }
 
     public function send($event) {
-        die(var_dump($event));
+        try {
+            Yii::$app->partnerMailer->compose('partner-request-manager', [
+                'partnerRequest' => Yii::$app->request->post('PartnerRequest'),
+                'profile' => Yii::$app->request->post('Profile')
+            ])
+                ->setFrom($event->sender->module->senderEmail)
+                ->setTo($event->sender->module->partnerManagerEmail)
+                ->setSubject(Yii::t('shop', 'New partner request'))
+                ->send();
+        } catch (Exception $ex) {
+            throw new Exception($ex);
+        }
+
+        try {
+            Yii::$app->partnerMailer->compose('partner-request-partner', [
+                'partnerRequest' => Yii::$app->request->post('PartnerRequest'),
+            ])
+                ->setFrom($event->sender->module->senderEmail)
+                ->setTo(Yii::$app->request->post('register-form')['email'])
+                ->setSubject(Yii::t('shop', 'Partner request'))
+                ->send();
+        } catch (Exception $ex) {
+            throw new Exception($ex);
+        }
     }
 
 }
