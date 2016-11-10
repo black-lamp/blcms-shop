8338797ab432bc239467543c0ce53237baf6fc4b
xalberteinsteinx <xalbert.einsteinx@gmail.com>
2016-11-10 12:35:54 +0200

    Adds email properties to frontend Module class.

--- frontend/Module.php
+++ frontend/Module.php
@@ -1,14 +1,24 @@
 <?php
-
 namespace bl\cms\shop\frontend;
 
-use bl\cms\cart\CartComponent;
-
+/**
+ * @author Albert Gainutdinov <xalbert.einsteinx@gmail.com>
+ */
 class Module extends \yii\base\Module
 {
     public $controllerNamespace = 'bl\cms\shop\frontend\controllers';
 
-    public $cartConfig;
+    /**
+     * @var string
+     * Partner manager e-mail, on which partner request will be sent.
+     */
+    public $partnerManagerEmail;
+
+    /**
+     * @var string
+     * From this e-mail partner request will be sent to Partner manager and partner.
+     */
+    public $senderEmail;
 
     public function init()
     {
