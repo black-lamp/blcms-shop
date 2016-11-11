0d9e2322fbe700559b86a0b4381ecb03d9f85556
xalberteinsteinx <xalbert.einsteinx@gmail.com>
2016-11-11 14:56:10 +0200

    Fixes migration.

--- migrations/m161110_152012_add_favorite_products_table.php
+++ migrations/m161110_152012_add_favorite_products_table.php
@@ -13,9 +13,9 @@ class m161110_152012_add_favorite_products_table extends Migration
         ]);
 
         $this->addForeignKey('shop_favorite_product_product_id:shop_product_id',
-            'shop_favorite_product', 'product_id', 'shop_product', 'id');
+            'shop_favorite_product', 'product_id', 'shop_product', 'id', 'cascade', 'cascade');
         $this->addForeignKey('shop_favorite_product_user_id:user_id',
-            'shop_favorite_product', 'user_id', 'user', 'id');
+            'shop_favorite_product', 'user_id', 'user', 'id', 'cascade', 'cascade');
     }
 
     public function down()
