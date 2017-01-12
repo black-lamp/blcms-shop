3fa5ace1fef25b9210976eb4e12b8c8ea686d434
xalberteinsteinx <xalbert.einsteinx@gmail.com>
2017-01-12 21:24:50 +0200

    Views from pools gallery.

---
+++ frontend/views/cart/index.php
@@ -0,0 +1,128 @@
+<?php
+/**
+ * Created by Albert Gainutdinov
+ */
+
+use yii\helpers\Url;
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+$this->title = 'Кошик';
+
+?>
+
+<h1 class="pull-left"><?= Yii::t('frontend/shop/cart', 'Корзина'); ?></h1>
+
+<? if(!empty($products)) : ?>
+    <a href="<?= Url::to(['/shop/cart/clear']) ?>" class="pull-right btn btn-primary clear-btn">
+        <?= Yii::t('frontend/shop/cart', 'Очистить корзину'); ?>
+    </a>
+<? endif ?>
+
+
+
+<div class="cart">
+    <? if(!empty($products)) : ?>
+        <? foreach($products as $key => $product) : ?>
+            <div class="item">
+
+                <span>
+                    <?= $key + 1 ?>
+                </span>
+
+                <h4>
+                    <a href="<?= Url::toRoute(['/shop/product/show?id=' . $product['id']]) ?>">
+                        <?= $product['title'] ?>
+                    </a>
+                </h4>
+
+                <div class="content">
+
+                    <div class="params">
+
+                        <div class="tara">
+                            <span>
+                                <?= $product['tara'] ?> x <?=$product['count'];?>
+                            </span>
+                        </div>
+
+                        <div class="price">
+                            <span>
+                                <?= $product['price'] ?> грн.
+                            </span>
+                        </div>
+                    </div>
+
+                    <div class="image">
+                        <img src="/admin/upload/shop-images/<?= $product['imageFile'] ?>" alt="">
+                    </div>
+
+                </div>
+
+            </div>
+        <? endforeach ?>
+
+    <p class="total-sum">Итого: <?= $totalSum; ?></p>
+    <div class="row">
+        <div class="col-md-12 text-center">
+            <a href="#" class="btn btn-primary btn-lg" id="order-btn">
+                <?= Yii::t('frontend/shop/cart', 'Оформить заказ'); ?>
+            </a>
+        </div>
+    </div>
+
+    <div class="row" id="order-form" style="display:none;">
+        <div class="col-md-12">
+
+            <div class="col-md-6">
+
+                <? $createOrder = ActiveForm::begin([
+                    'action' => Url::to(['']),
+                    'method' => 'post',
+                ]) ?>
+
+                <div class="form-group">
+                    <?= $createOrder->field($order, 'name', [
+                        'inputOptions' => [
+                            'class' => 'form-control'
+                        ]
+                    ])->label(Yii::t('frontend/shop/order', 'Имя')) ?>
+                </div>
+
+                <div class="form-group">
+                    <?= $createOrder->field($order, 'email', [
+                        'inputOptions' => [
+                            'class' => 'form-control'
+                        ]
+                    ])->label('Email') ?>
+                </div>
+
+                <div class="form-group">
+                    <?= $createOrder->field($order, 'phone', [
+                        'inputOptions' => [
+                            'class' => 'form-control'
+                        ]
+                    ])->label('Телефон') ?>
+                </div>
+
+                <?= Html::submitButton(Yii::t('frontend/shop/order', 'Отправить'), ['class' => 'btn btn-primary pull-right']) ?>
+
+                <? ActiveForm::end() ?>
+
+            </div>
+
+        </div>
+    </div>
+</div>
+
+
+
+<? else : ?>
+
+    <p>
+        <?= Yii::t('frontend/shop/cart', 'У Вас пока ещё нет товаров'); ?>.
+        <br>
+        <?= Yii::t('frontend/shop/cart', 'Вернитесь в'); ?> <a href="<?= Url::to(['/shop']) ?>"><?= Yii::t('frontend/shop/cart', 'магазин'); ?></a>, <?= Yii::t('frontend/shop/cart', 'чтобы выбрать товар'); ?>.
+    </p>
+
+<? endif ?>
---
+++ frontend/views/category/show.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * @author Albert Gainutdinov <xalbert.einsteinx@gmail.com>
+ *
+ * @var $menuItems Category
+ * @var $category Category
+ * @var $products Product
+ */
+
+use bl\multilang\entities\Language;
+use bl\cms\shop\common\entities\Category;
+use bl\cms\shop\common\entities\Product;
+use yii\helpers\Url;
+?>
+
+<div class="row products">
+    <div class="col-md-12">
+        <div class="col-md-3 menu-categories">
+            <?php foreach ($menuItems as $menuItem) : ?>
+                <p>
+                    <a href="<?= Url::toRoute(['category/show', 'categoryId' => $menuItem->id]); ?>" >
+                        <?= $menuItem->translation->title ?>
+                    </a>
+                </p>
+            <? endforeach; ?>
+        </div>
+        <div class="col-md-9">
+            <h1><?=$category->translation->title; ?></h1>
+            <? foreach ($products as $product): ?>
+                <div class="col-md-4 text-center product">
+                    <a href="<?= Url::to(['product/show', 'id' => $product->id])?>">
+                        <div class="img">
+                            <img src="/admin/upload/gallery/154.jpg" alt="" width="">
+                        </div>
+                        <div class="content">
+                            <div class="cell">
+                                <span class="title">
+                                    <?= $product->translation->title ?>
+                                </span>
+                                <span class="price">
+                                    <span class="new">
+                                        <?= $product->price; ?>
+                                    </span>
+                                </span>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+            <? endforeach; ?>
+        </div>
+    </div>
+</div>
+
