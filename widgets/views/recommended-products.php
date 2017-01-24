96f96be93a235927211b982066713270973d4b0c
xalberteinsteinx <xalbert.einsteinx@gmail.com>
2017-01-24 20:37:02 +0200

    Changes RecommendedProducts widget view.

--- widgets/views/recommended-products.php
+++ widgets/views/recommended-products.php
@@ -5,6 +5,7 @@
  * @var $recommendedProducts \bl\cms\shop\common\entities\Product
  */
 use bl\cms\cart\models\CartForm;
+use bl\cms\shop\frontend\widgets\ProductPrices;
 use yii\bootstrap\ActiveForm;
 use yii\bootstrap\Html;
 use yii\helpers\ArrayHelper;
@@ -13,7 +14,7 @@ use yii\helpers\Url;
 ?>
 
 <?php if (!empty($recommendedProducts)) : ?>
-    <div class="col-md-12 recommended-products">
+    <div class="col-md-12 recommended-products product">
 
         <!--WIDGET TITLE-->
         <p class="recommended-products-title">
@@ -53,36 +54,11 @@ use yii\helpers\Url;
                         </a>
                     </p>
 
-                    <div class="price-and-count">
-                        <!--Price-->
-                        <div class="price col-md-6">
-                            <?php if (!empty($recommendedProduct->prices)) : ?>
-                                <?= $form->field($cart, 'priceId', ['options' => ['class' => '']])->dropDownList(ArrayHelper::map($recommendedProduct->prices, 'id', function ($recommendedProduct) {
-                                    $priceItem = $recommendedProduct->translation->title . ' - ' . \Yii::$app->formatter->asCurrency($recommendedProduct->price);
-                                    return $priceItem;
-                                }))->label(\Yii::t('shop', 'Price')); ?>
-                            <?php else : ?>
-                                <p class="label-price"><?= Yii::t('shop', 'Price'); ?></p>
-                                <p class="standart-price">
-                                    <?= \Yii::$app->formatter->asCurrency($recommendedProduct->price); ?>
-                                </p>
-                            <?php endif; ?>
-                            <?= $form->field($cart, 'productId')->hiddenInput(['value' => $recommendedProduct->id])->label(false); ?>
-                        </div>
-
-                        <!--Count-->
-                        <div class="count col-md-6">
-                            <?= $form->field($cart, 'count', ['options' => ['class' => '']])->
-                            textInput(['type' => 'number', 'min' => 1, 'value' => 1])->label(\Yii::t('shop', 'Count'));
-                            ?>
-                        </div>
-                    </div>
-
                     <!--Button-->
                     <div class="buy">
-                        <?= Html::submitButton(
-                            Html::tag('span', '', ['class' => 'fa fa-shopping-cart']),
-                            ['class' => 'btn btn-tight btn-primary']); ?>
+                        <a href="<?= Url::to(['/shop/product/show', 'id' => $recommendedProduct->id]); ?>" class="btn btn-tight btn-primary">
+                            <?= Html::tag('span', '', ['class' => 'fa fa-shopping-cart']); ?>
+                        </a>
                     </div>
                 </div>
                 <?php $form::end(); ?>
